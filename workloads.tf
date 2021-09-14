188b196ec1f0881ee94d1b47d40f7a37cf9b145b
John Smoker <jsmoker@aviatrix.com>
2021-09-14 07:54:44 -0700

    remove workload count for consistency

--- workloads.tf
+++ workloads.tf
@@ -31,7 +31,6 @@ resource "aviatrix_gateway" "us_east_1_psf" {
 # }
 
 # module "workload_instance_eu_central_1" {
-#   count            = contains(keys(local.region), "eu-central-1") ? 1 : 0
 #   source           = "./workload_instance"
 #   keypair_name     = "smoker"
 #   subnet_id        = module.regional_network["eu-central-1"].spoke_2_vpc.private_subnets[0].subnet_id
@@ -44,7 +43,6 @@ resource "aviatrix_gateway" "us_east_1_psf" {
 # }
 
 # module "workload_instance_ap_southeast_2" {
-#   count            = contains(keys(local.region), "ap-southeast-2") ? 1 : 0
 #   source           = "./workload_instance"
 #   keypair_name     = "smoker"
 #   subnet_id        = module.regional_network["ap-southeast-2"].spoke_2_vpc.private_subnets[0].subnet_id
