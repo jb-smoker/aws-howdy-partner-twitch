eab77dc783dc4d08643e661591548a059c04fe1b
John Smoker <jsmoker@aviatrix.com>
2021-09-15 15:33:47 -0700

    updated fqdn rules

--- egress_fqdn/fqdn-rules.tf
+++ egress_fqdn/fqdn-rules.tf
@@ -4,11 +4,11 @@ locals {
     tcp = {
       "*.amazonaws.com" = "443"
       "*.aviatrix.com"  = "443"
-      # "*.terraform.io"  = "443"
-      # "*.onug.net"      = "443"
+      "*.terraform.io"  = "443"
+      "*.onug.net"      = "443"
     }
     udp = {
-      # "dns.google.com" = "53"
+      "dns.google.com" = "53"
     }
   }
 }
